d12bbc5babd934c7c529d5919683e73e0281b49c
John Watson <johnwatson484@hotmail.com>
2022-01-02 00:17:41 +0000

    Switch to use stream

--- app/index.js
+++ app/index.js
@@ -1,22 +1,29 @@
 const cache = require('./cache')
-const wreck = require('@hapi/wreck')
+const fs = require('fs')
+const https = require('https')
 const csv = require('csvtojson')
 
 const main = async () => {
   console.log('Sourcing data')
-  await cache.start()
-  const { payload } = await wreck.get('https://opensky-network.org/datasets/metadata/aircraftDatabase.csv')
-  const stream = wreck.toReadableStream(payload)
-  const aircraft = await csv().fromStream(stream)
 
-  for (const plane of aircraft) {
-    if (plane.icao24) {
-      await cache.update('data', plane.icao24, plane)
-    }
-  }
+  const file = fs.createWriteStream('/tmp/aircraft-database.csv')
+  https.get('https://opensky-network.org/datasets/metadata/aircraftDatabase.csv', async function (response) {
+    response.pipe(file)
 
-  await cache.stop()
-  console.log('Data refreshed')
+    response.on('end', async function () {
+      await cache.start()
+      const aircraft = await csv().fromFile('/tmp/aircraft-database.csv')
+
+      for (const plane of aircraft) {
+        if (plane.icao24) {
+          await cache.update('data', plane.icao24, plane)
+        }
+      }
+
+      await cache.stop()
+      console.log('Data refreshed')
+    })
+  })
 }
 
 for (const signal of ['SIGINT', 'SIGTERM', 'SIGQUIT']) {
