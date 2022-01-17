40307d4c22ac8b12d6f8377616b87a53ac9c9040
John Watson <johnwatson484@hotmail.com>
2022-01-17 18:11:50 +0000

    Fix ttl

--- app/config.js
+++ app/config.js
@@ -12,7 +12,7 @@ const schema = Joi.object().keys({
     }),
     password: Joi.string().allow(''),
     partition: Joi.string().default('aircraft-cache'),
-    ttl: Joi.number().default(3600 * 1000 * 24 * 30) // 30 days
+    ttl: Joi.number().default(2592000) // 30 days
   })
 })
 
